1985c5475c9cf97ca3e9c431f76017efae3f53c6
Prashant <prashant9154@gmail.com>
2023-01-16 16:00:11 +0530

    removed build in git ignore

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use("/api",video);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "../frontend/build"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 })
 
 //middleware for errors
